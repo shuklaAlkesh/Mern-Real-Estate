97299c907b709c1dc2f5c89a13626aab0951950e
Alkesh shukla <shukla2002alkesh@gmail.com>
2024-07-12 23:28:41 +0530

    create signIn Api route

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link,useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
+  
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      // if (!res.ok) {
+      //   throw new Error('Network Issues');
+      // }
+
+      const data = await res.json();
+      if(data.success == false){
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/'); 
+      // console.log(data); // For debugging purposes
+    } catch (error) {
+      setLoading(false);
+      console.error('There was a problem with the fetch operation:', error);
+      setError(error.message);
+    }
+  };
+
   return (
-    <div>signIn</div>
-  )
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      {error && <p className="text-red-500 text-center">{error}</p>}
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type="text"
+          placeholder='Username'
+          className='border p-3 rounded-lg'
+          id='username'
+          value={formData.username}
+          onChange={handleChange}
+        />
+
+        <input
+          type="email"
+          placeholder='Email'
+          className='border p-3 rounded-lg'
+          id='email'
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <input
+          type="password"
+          placeholder='Password'
+          className='border p-3 rounded-lg'
+          id='password'
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <button
+          disabled={loading}
+          type="submit"
+          className='bg-slate-700 
+          hover:text-white 
+          p-3 rounded-lg uppercase 
+          hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+      </form>
+
+      <div className='flex gap-2 mt-5'>
+        <p>Don't Have an account?</p>
+        <Link to="/sign-up">
+          <span className='text-blue-700'>Sign Up</span>
+        </Link>
+      </div>
+      {/* {error &&<p className='text-red-500 mt-5'>{error}</p>} */}
+    </div>
+  );
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -53,7 +53,7 @@ export default function SignUp() {
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign-Up</h1>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       {error && <p className="text-red-500 text-center">{error}</p>}
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
